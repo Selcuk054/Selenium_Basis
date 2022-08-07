0852b465db9be87a25c3ae63eb6d99bdc64c2c80
Selcuk Kardes <selcukkrds09@gmai.com>
2022-08-07 19:24:54 +0200

    07.08

---
+++ src/day01/C01_IlkTest.java
@@ -0,0 +1,36 @@
+package day01;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C01_IlkTest {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","/Users/selcukkardes/Documents/Selenium Libraries/Drivers/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.com/");
+        System.out.println("suandaki url : "+driver.getCurrentUrl());
+        System.out.println("su anki baslik : "+ driver.getTitle());
+        driver.manage().window().maximize();
+        Thread.sleep(3000);
+
+        driver.navigate().to("https://www.techproeducation.com");
+        Thread.sleep(2000);
+        driver.navigate().back();
+        Thread.sleep(2000);
+
+
+        driver.navigate().forward();
+        Thread.sleep(2000);
+
+        driver.navigate().refresh();
+        Thread.sleep(2000);
+
+        // driver.quit(); quit acik olan tum browser'lari kapatirken close sadece 1 browser kapatir
+
+
+        driver.close();
+
+    }
+
+}
